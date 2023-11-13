580dcfacb42cc5e395dd936b08fe4309cc0e39f3
v1enor <ilya33113@gmail.com>
2023-11-13 10:18:22 +0300

    я мобилизован

---
+++ MS/lab10/test/unit_test.dart
@@ -0,0 +1,33 @@
+import 'package:firebaseapp/second_simple.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockController extends Mock implements TextEditingController {}
+
+void main() {
+  testWidgets('Test Calculator App', (WidgetTester tester) async {
+    final MockController mockController = MockController();
+
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(home: CalculatorScreen()));
+
+    // Verify that our app has a TextField
+    expect(find.byType(TextField), findsOneWidget);
+
+    // Enter a number in the TextField
+    await tester.enterText(find.byType(TextField), '5');
+
+    // Verify that the TextField has the entered text
+    expect(find.text('5'), findsOneWidget);
+
+    // Trigger the button press
+    await tester.tap(find.byType(ElevatedButton));
+
+    // Wait for animations to complete
+    await tester.pumpAndSettle();
+
+    // Verify that the result is displayed
+    expect(find.text('Result: 10'), findsOneWidget);
+  });
+}
---
+++ MS/lab10/test/widget_test.dart
@@ -0,0 +1,16 @@
+import 'package:firebaseapp/simple_widget.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  testWidgets('HelloWorld widget shows correct text', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: HelloWorldWidget()));
+
+    final textFinder = find.text('Hello, World!');
+    expect(textFinder, findsOneWidget);
+
+    Text textWidget = tester.widget(textFinder);
+    expect(textWidget.data, 'Hello, World!');
+  });
+
+}
