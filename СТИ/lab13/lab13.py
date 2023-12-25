7f0b99682e90bda42a89f4098b41a9bec77621c4
ilusha <ilya33113@gmail.com>
2023-12-25 15:25:32 +0300

    пук пук

--- СТИ/lab13/lab13.py
+++ СТИ/lab13/lab13.py
@@ -1,37 +1,55 @@
 import cv2
 import mediapipe as mp
 
-mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
 
-# For webcam input:
+# Load the overlay image: replace 'overlay.png' with your image path
+overlay = cv2.imread("lab13/water.jpg")
+# Resize the image to fit the video frame
+overlay = cv2.resize(overlay, (20, 20))  # Smaller size for finger tips
+
+# Create a VideoCapture object
 cap = cv2.VideoCapture(0)
-with mp_hands.Hands(
-        min_detection_confidence=0.5,
-        min_tracking_confidence=0.5) as hands:
-    while cap.isOpened():
-        success, image = cap.read()
-        if not success:
-            print("Ignoring empty camera frame.")
-            continue
 
-        # Flip the image horizontally for a later selfie-view display, and convert
-        # the BGR image to RGB.
-        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-        # To improve performance, optionally mark the image as not writeable to
-        # pass by reference.
-        image.flags.writeable = False
-        results = hands.process(image)
-        
-        # Draw the hand annotations on the image.
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                        image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-        cv2.imshow('MediaPipe Hands', image)
-        if cv2.waitKey(5) & 0xFF == 27:
-            break
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+while True:
+    success, image = cap.read()
+    if not success:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    # pass by reference.
+    image.flags.writeable = False
+    results = hands.process(image)
+
+    # Draw the hand annotations on the image.
+    image.flags.writeable = True
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            # Get the coordinates of the tip of each finger
+            for finger in [mp_hands.HandLandmark.THUMB_TIP, mp_hands.HandLandmark.INDEX_FINGER_TIP, mp_hands.HandLandmark.MIDDLE_FINGER_TIP, mp_hands.HandLandmark.RING_FINGER_TIP, mp_hands.HandLandmark.PINKY_TIP]:
+                x = int(hand_landmarks.landmark[finger].x * image.shape[1])
+                y = int(hand_landmarks.landmark[finger].y * image.shape[0])
+                
+                # Get the height and width of the overlay
+                h, w = overlay.shape[:2]
+
+                # Ensure y and x are within the bounds that can accommodate the overlay
+                y = min(y, image.shape[0] - h)
+                x = min(x, image.shape[1] - w)
+
+                # Check if there's enough space to place the overlay
+                if x >= 0 and y >= 0 and x + w <= image.shape[1] and y + h <= image.shape[0]:
+                    # Overlay the image at the tip of the index finger
+                    image[y:y+h, x:x+w] = overlay
+
+    cv2.imshow('MediaPipe Hands', image)
+    if cv2.waitKey(5) & 0xFF == 27:
+        break
+
 cap.release()
-
+cv2.destroyAllWindows()
